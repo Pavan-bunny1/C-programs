2161d280e068200a154fb4196d42dc1fe242885e
A Pavan <pavankumar8275@gmail.com>
2025-02-10 20:37:38 +0530

    Update Goto-stmt.c

--- Goto-stmt.c
+++ Goto-stmt.c
@@ -18,3 +18,26 @@ int main() {
     end:
         return 0;
 }
+
+
+
+//odd or even
+#include <stdio.h>
+main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    check_even_odd:
+    goto even;
+    
+    even:
+        if(num % 2 == 0)
+        {
+            printf("%d is even.\n", num);
+        }
+        else
+        {
+            printf("%d is odd.\n", num);
+        }
+}
