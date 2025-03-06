92bcd4e80da1768512e4cd45179cbe87ff029134
A Pavan <pavankumar8275@gmail.com>
2025-03-06 12:19:48 +0530

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+Main()
+{
+char ch;
+int a,b;
+printf("a.addition");
+printf("b.substraction");
+printf("c.multiplication");
+printf("d.divdion");
+printf("e.modules");
+printf("enter your choice");
+scanf("%c",&ch);
+printf("enter your 1st and 2nd values");
+scanf("%d%d",&a,&b);
+switch(ch)
+{
+case'a':
+printf("%d%d%d",a,b,a+b);
+break;
+case'b':
+printf("%d%d%d",a,b,a-b);
+break;
+case'c':
+printf("%d%d%d",a,b,a*b);
+break;
+case'd':
+printf("%d%d%d",a,b,a/b);
+break;
+case'e':
+printf("%d%d%d",a,b,a%b);
+break;
+default:
+printf("invalid input");
+}
+}
